afca6990586997efc44a3237365cd7ebdb06d56f
Albert Cervin <albert.cervin@goodbyekansas.com>
2019-10-28 13:00:41 +0100

    Fixup the USD Core Nix derivation so that it actually works

--- usd-core.nix
+++ usd-core.nix
@@ -1,18 +1,9 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
-nixpkgsa = import <nixpkgs> {
-   overlays = [ (self: super: {
-      glibc = super.glibc_multi.overrideAttrs (old: rec {
-        src = super.fetchurl {
-          url = "mirror://gnu/glibc/glibc-2.19.tar.gz";
-          sha256 = "15n7x9mmzhd7w6s5hd9srx0h23b32gwb306x98k9ss940yvnvb8q";
-        };
-      });
-   })];
-  };
+  pyenv = (nixpkgs.python27.withPackages (ps: with ps; [ pyside2 jinja2 pyopengl pyside2-tools ]));
 in
 with nixpkgs;
-(overrideCC stdenv gcc6).mkDerivation {
+gcc6Stdenv.mkDerivation {
   name = "USD-Core";
   nativeBuildInputs = [
     cmake
@@ -21,18 +12,16 @@ with nixpkgs;
 
   buildInputs = [ 
     glew
-    nixpkgsa.glibc
     xorg.libXxf86vm
     xorg.libXrandr
     xorg.libXcursor
     xorg.libXinerama
     xorg.libXi
     zlib
-    python27
-    python27Packages.jinja2
-    python27Packages.pyside2
-    python27Packages.pyside2-tools
-    python27Packages.pyopengl
+  ];
+
+  propagatedBuildInputs = [
+    pyenv
   ];
 
   src = fetchFromGitHub {
@@ -44,8 +33,8 @@ with nixpkgs;
 
   phases = [ "unpackPhase" "buildPhase" ];
 
+  TBB_PLATFORM=linux;
   buildPhase = ''
-    export TBB_PLATFORM=linux
-    ${python27}/bin/python build_scripts/build_usd.py --build-args boost,"--with-date_time --with-thread --with-system --with-filesystem" --no-maya -v $out/
+    ${pyenv}/bin/python build_scripts/build_usd.py --build-args boost,"--with-date_time --with-thread --with-system --with-filesystem" --no-maya -v $out/
   '';
 }
